eaab2617d2223a2d2c5d2ebaa213b2cef1fa1df0
YangusKhan <9inchsamurai@gmail.com>
2016-07-10 00:04:38 -0600

    The page now stores spreadsheet values into a JSON string and then stores it in localStorage. It then checks for this object in localStorage during initialization, to speed up page loading.

--- dungeon_parser.js
+++ dungeon_parser.js
@@ -2,18 +2,34 @@ var spreadsheet_id = '16K1Zryyxrh7vdKVF1f7eRrUAOC5wuzvC3q2gFLch6LQ';
 var sheet_id = '1319409797';
 var API_key = 'AIzaSyDeDLSSUqXfAUyEClwceGUWPhbjJqU-IfM';
 function initAuth() {
-    gapi.client.setApiKey(API_key);
-    gapi.client.load('sheets', 'v4').then(getSheet);
+    var master_list = localStorage.getItem('session');
+    if (master_list === null) {
+	gapi.client.setApiKey(API_key);
+	gapi.client.load('sheets', 'v4').then(getSheet);
+    } else {
+	var values = JSON.parse(master_list);
+	parseJSON(values);
+    }
 }
 function getSheet() {
     gapi.client.sheets.spreadsheets.values.get({
 	'spreadsheetId': spreadsheet_id,
 	'range': 'Dungeons!A2:L324',
-    }).then(parseSheet);
+    }).then(parseGoogleResponse);
 }
-function parseSheet(response) {
+function parseGoogleResponse(response) {
     var values = response.result.values;
+    parseArray(values);
+}
+function parseJSON(json) {
     var table = document.getElementById("dungeon-table");
+    json.forEach(function(element, index, array) {
+	insertRow(table.tBodies[0], element);
+    });
+}
+function parseArray(values) {
+    var table = document.getElementById("dungeon-table");
+    var master_list = [];
     values.forEach(function(element, index, array) {
 	var realm = element[0];
 	var name = element[1];
@@ -21,15 +37,14 @@ function parseSheet(response) {
 	var EliteObj = { 'stamina': element[7], 'first_time': element[10], 'mastery': element[11], 'reward': 0 };
 	var check_elite = EliteObj['first_time'] + "," + EliteObj['mastery'];
         var check_normal = ClassicObj['first_time'] + "," + ClassicObj['mastery'];
-        var expression = /Stamina Shard x([0-9])/;
-        var match = expression.exec(check_normal);
-	if (match !== null) {
+        var expression = /(?:Stamina Shard x([0-9]))+/g;
+        var match;
+	while ((match = expression.exec(check_normal)) !== null) {
 	   for (var i = 1; i < match.length; i++) {
 	       ClassicObj['reward'] += parseInt(match[i]);
 	   }
 	}
-	match = expression.exec(check_elite);
-	if (match !== null) {
+	while ((match = expression.exec(check_elite)) !== null) {
 	   for (var i = 1; i < match.length; i++) {
 	       EliteObj['reward'] += parseInt(match[i]);
 	   }
@@ -38,7 +53,9 @@ function parseSheet(response) {
         var ratio_elite = (EliteObj['reward'] == 0) ?  0.0: (parseFloat(EliteObj['reward']) / parseFloat(EliteObj['stamina']));
 	var RowObj = { 'realm': realm, 'name': name, 'classic': ClassicObj, 'elite': EliteObj };
 	insertRow(table.tBodies[0], RowObj);
+	master_list.push(RowObj);
     });
+    localStorage.setItem('session', JSON.stringify(master_list));
 }
 
 function insertRow(table, RowObj) {
@@ -54,17 +71,21 @@ function insertRow(table, RowObj) {
 
     newCell = row.insertCell();
     var costText = document.createTextNode(RowObj['classic']['stamina']);
+    newCell.className = "classic-cell";
     newCell.appendChild(costText);
 
     newCell = row.insertCell();
     var rewardText = document.createTextNode(RowObj['classic']['reward']);
+    newCell.className = "classic-cell";
     newCell.appendChild(rewardText);
 
     newCell = row.insertCell();
     var costTextElite = document.createTextNode(RowObj['elite']['stamina']);
+    newCell.className = "elite-cell";
     newCell.appendChild(costTextElite);
 
     newCell = row.insertCell();
     var rewardTextElite = document.createTextNode(RowObj['elite']['reward']);
+    newCell.className = "elite-cell";
     newCell.appendChild(rewardTextElite);
 }
