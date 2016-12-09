3450d1d81cc4eb14e9608a5f8fffccf6f78e19b5
YangusKhan <9inchsamurai@gmail.com>
2016-12-09 00:56:39 -0700

    Added preliminary filters and styles for them. Also helper function for clearing cached spreadsheet values.

--- dungeon_parser.js
+++ dungeon_parser.js
@@ -4,17 +4,22 @@ var API_key = 'AIzaSyDeDLSSUqXfAUyEClwceGUWPhbjJqU-IfM';
 function initAuth() {
     var master_list = localStorage.getItem('session');
     if (master_list === null) {
-	gapi.client.setApiKey(API_key);
-	gapi.client.load('sheets', 'v4').then(getSheet);
+	    gapi.client.setApiKey(API_key);
+	    gapi.client.load('sheets', 'v4').then(getSheet);
     } else {
-	var values = JSON.parse(master_list);
-	parseJSON(values);
+	    var values = JSON.parse(master_list);
+	    parseJSON(values);
     }
+}
+function reloadSheet() {
+    localStorage.removeItem('session');
+    gapi.client.setApiKey(API_key);
+	  gapi.client.load('sheets', 'v4').then(getSheet);
 }
 function getSheet() {
     gapi.client.sheets.spreadsheets.values.get({
-	'spreadsheetId': spreadsheet_id,
-	'range': 'Dungeons!A2:L324',
+	    'spreadsheetId': spreadsheet_id,
+	    'range': 'Dungeons!A2:L324',
     }).then(parseGoogleResponse);
 }
 function parseGoogleResponse(response) {
@@ -24,36 +29,38 @@ function parseGoogleResponse(response) {
 function parseJSON(json) {
     var table = document.getElementById("dungeon-table");
     json.forEach(function(element, index, array) {
-	insertRow(table.tBodies[0], element);
+	    insertRow(table.tBodies[0], element);
     });
 }
 function parseArray(values) {
     var table = document.getElementById("dungeon-table");
     var master_list = [];
     values.forEach(function(element, index, array) {
-	var realm = element[0];
-	var name = element[1];
-	var ClassicObj = { 'stamina': element[2], 'first_time': element[5], 'mastery': element[6], 'reward': 0 };
-	var EliteObj = { 'stamina': element[7], 'first_time': element[10], 'mastery': element[11], 'reward': 0 };
-	var check_elite = EliteObj['first_time'] + "," + EliteObj['mastery'];
-        var check_normal = ClassicObj['first_time'] + "," + ClassicObj['mastery'];
-        var expression = /(?:Stamina Shard x([0-9]))+/g;
-        var match;
-	while ((match = expression.exec(check_normal)) !== null) {
-	   for (var i = 1; i < match.length; i++) {
-	       ClassicObj['reward'] += parseInt(match[i]);
-	   }
-	}
-	while ((match = expression.exec(check_elite)) !== null) {
-	   for (var i = 1; i < match.length; i++) {
-	       EliteObj['reward'] += parseInt(match[i]);
-	   }
-	}
-        var ratio_normal = (ClassicObj['reward'] == 0) ? 0.0 :  (parseFloat(ClassicObj['reward']) / parseFloat(ClassicObj['stamina']));
-        var ratio_elite = (EliteObj['reward'] == 0) ?  0.0: (parseFloat(EliteObj['reward']) / parseFloat(EliteObj['stamina']));
-	var RowObj = { 'realm': realm, 'name': name, 'classic': ClassicObj, 'elite': EliteObj };
-	insertRow(table.tBodies[0], RowObj);
-	master_list.push(RowObj);
+	    var realm = element[0];
+	    var name = element[1];
+	    var ClassicObj = { 'stamina': element[2], 'first_time': element[5], 'mastery': element[6], 'reward': 0 };
+	    var EliteObj = { 'stamina': element[7], 'first_time': element[10], 'mastery': element[11], 'reward': 0 };
+	    var check_elite = EliteObj['first_time'] + "," + EliteObj['mastery'];
+      var check_normal = ClassicObj['first_time'] + "," + ClassicObj['mastery'];
+      var expression = /(?:Stamina Shard x([0-9]))+/g;
+      var match;
+	    while ((match = expression.exec(check_normal)) !== null) {
+	       for (var i = 1; i < match.length; i++) {
+	           ClassicObj['reward'] += parseInt(match[i]);
+	       }
+	    }
+	    while ((match = expression.exec(check_elite)) !== null) {
+	       for (var i = 1; i < match.length; i++) {
+	           EliteObj['reward'] += parseInt(match[i]);
+	       }
+	    }
+      var ratio_normal = (ClassicObj['reward'] == 0) ? 0.0 :  (parseFloat(ClassicObj['reward']) / parseFloat(ClassicObj['stamina']));
+      var ratio_elite = (EliteObj['reward'] == 0) ?  0.0: (parseFloat(EliteObj['reward']) / parseFloat(EliteObj['stamina']));
+      ClassicObj['ratio'] = ratio_normal;
+      EliteObj['ratio'] = ratio_elite;
+	    var RowObj = { 'realm': realm, 'name': name, 'classic': ClassicObj, 'elite': EliteObj };
+	    insertRow(table.tBodies[0], RowObj);
+	    master_list.push(RowObj);
     });
     localStorage.setItem('session', JSON.stringify(master_list));
 }
@@ -78,6 +85,11 @@ function insertRow(table, RowObj) {
     var rewardText = document.createTextNode(RowObj['classic']['reward']);
     newCell.className = "classic-cell";
     newCell.appendChild(rewardText);
+    
+    newCell = row.insertCell();
+    var rewardText = document.createTextNode(RowObj['classic']['ratio']);
+    newCell.className = "classic-cell";
+    newCell.appendChild(rewardText);
 
     newCell = row.insertCell();
     var costTextElite = document.createTextNode(RowObj['elite']['stamina']);
@@ -86,6 +98,11 @@ function insertRow(table, RowObj) {
 
     newCell = row.insertCell();
     var rewardTextElite = document.createTextNode(RowObj['elite']['reward']);
+    newCell.className = "elite-cell";
+    newCell.appendChild(rewardTextElite);
+    
+    newCell = row.insertCell();
+    var rewardTextElite = document.createTextNode(RowObj['elite']['ratio']);
     newCell.className = "elite-cell";
     newCell.appendChild(rewardTextElite);
 }
